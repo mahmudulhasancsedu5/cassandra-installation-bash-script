a3d9acd054af9a290b6503f3a223fa67b014e8aa
Mahmudul Hasan <hasancsedu5@gmail.com>
2023-09-19 20:23:24 +0600

    1. Add depart install and configure bash scripts

---
+++ depart_configure_script.sh
@@ -0,0 +1,58 @@
+set -x
+
+# make dir for cassandra
+
+USER_HOME=`echo ~`
+
+DEPART_HOME="$USER_HOME/depart"
+
+# Cnfigure cassandra
+
+if [ ! -d $DEPART_HOME ]; then
+	echo "$DEPART_HOME: directory does not exist"
+	exit 1
+fi
+
+CASSANDRA_YAML="$DEPART_HOME/conf/cassandra.yaml"
+CASSANDRA_BASE_YAML="$DEPART_HOME/conf/cassandra-base.yaml"
+
+
+# make a backup copy of cassandra.yaml file
+
+
+if [ ! -f $CASSANDRA_BASE_YAML ]; then
+	echo "save a copy of original cassandra.yaml"
+	cp $CASSANDRA_YAML $CASSANDRA_BASE_YAML
+fi
+
+if [ -f $CASSANDRA_BASE_YAML ]; then
+	echo "copy  from cassandra-base.yaml for cassandra configuration setup"
+	cp $CASSANDRA_BASE_YAML $CASSANDRA_YAML
+fi
+
+# remove cassandra previous data
+rm -rf "$DEPART_HOME/data/*"
+
+IP_ADDRESS=`hostname -I` # for ec2 machine private ip will be cassandra node IP_ADDRESS
+SEEDS[0]='192.168.0.105'
+SEEDS[1]='192.168.0.103'
+
+# update cassandra configuration
+
+sudo sed -i "s/cluster_name: 'Depart Cluster'/cluster_name: 'AWS Depart Cluster'/g" $CASSANDRA_YAML
+sudo sed -i "s/listen_address: 192.168.0.103/listen_address: $IP_ADDRESS/g" $CASSANDRA_YAML
+sudo sed -i "s/rpc_address: 192.168.0.103/rpc_address: $IP_ADDRESS/g" $CASSANDRA_YAML
+
+
+function join { local IFS=","; shift; echo "$*"; }
+
+ALL_SEEDS=`join , ${SEEDS[@]}`
+
+sudo sed -i 's/- seeds: "192.168.0.103,192.168.0.105"/- seeds: "'${ALL_SEEDS[@]}'"/g' $CASSANDRA_YAML
+
+echo "depart setup: completed"
+echo "DEPART_HOME: $DEPART_HOME"
+echo "start depart cmd: bin/cassandra -f"
+echo "exit cassndra: (ctl + c)"
+
+
---
+++ depart_install_script.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+set -x
+
+#install wget
+WGET_PATH=`which curl`
+
+if [ -z $WGET_PATH ]; then
+	echo 'wget is not installed'
+	echo 'Install WGET_PATH'
+	sudo apt install wget -y
+else
+	echo 'wget is installed'
+fi
+
+# install unzip
+UNZIP_PATH=`which curl`
+
+if [ -z $UNZIP_PATH ]; then
+	echo 'unzip is not installed'
+	echo 'Install unzip'
+	sudo apt install unzip -y
+else
+	echo 'unzip is installed'
+fi
+
+#download depart
+
+# https://github.com/mahmudulhasancsedu5/cassandra-depart/archive/refs/heads/depart_multinode.zip
+DEPART_URL="https://github.com/mahmudulhasancsedu5/cassandra-depart/archive/refs/heads/depart_multinode.zip"
+
+USER_HOME=`echo ~`
+DEPART_HOME="$USER_HOME/depart"
+ZIP_FILE_NAME='depart_multinode.zip' # <git-branch-name>.git
+UNZIP_FOLDER_NAME='cassandra-depart-depart_multinode' # <git-reponame>-<git-branch-name>
+DEPART_FOLDER_NAME='depart'
+
+
+cd $USER_HOME
+
+#download depart
+ZIP_FILE="$USER_HOME/$ZIP_FILE_NAME"
+if [ ! -f $ZIP_FILE -a ! -d $DEPART_HOME ]; then
+	echo "$ZIP_FILE: downloading"
+	wget $DEPART_URL
+	echo "Download Completed"
+	
+else
+	echo "$ZIP_FILE already exist"
+fi
+
+
+if [ -f $ZIP_FILE -a ! -d "$USER_HOME/$UNZIP_FOLDER_NAME" ]; then
+	echo "unzip file"
+	unzip $ZIP_FILE
+	echo "unzip completed"
+fi
+
+if [ ! -d $DEPART_HOME ]; then
+	echo "rename $UNZIP_FOLDER_NAME to $DEPART_FOLDER_NAME"
+	mv "$USER_HOME/$UNZIP_FOLDER_NAME" "$DEPART_HOME"
+	echo "rename completed"
+fi
+
+
+echo "depart folder: $DEPART_HOME"
+
+
